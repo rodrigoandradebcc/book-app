844601a5a68a0645a817e1c908de2577d01c23d3
Rod Andrade <randrade@brbatel.com.br>
2021-07-26 18:03:34 -0300

    fix: configure styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,88 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+// import Document, { Head, Main, NextScript } from 'next/document';
+// import { ServerStyleSheet } from 'styled-components';
+
+// export default class MyDocument extends Document {
+//   static getInitialProps({ renderPage }: any) {
+//     const sheet = new ServerStyleSheet();
+
+//     const page = renderPage((App: any) => (props: any) =>
+//       sheet.collectStyles(<App {...props} />),
+//     );
+
+//     const styleTags = sheet.getStyleElement();
+
+//     return { ...page, styleTags };
+//   }
+
+//   render() {
+//     return (
+//       <html>
+//         <Head>
+//           <link
+//             rel="preload"
+//             href="/fonts/SFProDisplay/SF-Pro.ttf"
+//             as="font"
+//             crossOrigin=""
+//           />
+//           <title>Books</title>
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </html>
+//     );
+//   }
+// }
+
+
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }: any) {
-    const sheet = new ServerStyleSheet();
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    const page = renderPage((App: any) => (props: any) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
-    return (
-      <html>
-        <Head>
-          <link
-            rel="preload"
-            href="/fonts/SFProDisplay/SF-Pro.ttf"
-            as="font"
-            crossOrigin=""
-          />
-          <title>Books</title>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+        return (
+          <Html>
+            <Head>
+              <link
+                rel="preload"
+                href="/fonts/SFProDisplay/SF-Pro.ttf"
+                as="font"
+                crossOrigin=""
+              />
+              {/* <title>Books</title> */}
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+        );
+      }
 }
